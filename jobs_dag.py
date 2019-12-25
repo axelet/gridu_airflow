d6454c645f9d0827111300dff89efa4e1c3703b4
axelet <axelet3@gmail.com>
2019-12-25 19:58:52 +0300

    [GRID-AIRFLOW] 18. Add Branching operator

--- jobs_dag.py
+++ jobs_dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
-from airflow.models import Variable
+from airflow.models import Connection, Variable
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from datetime import datetime
 import logging
 
@@ -17,6 +17,10 @@ def process_db_table(dag_id, database, **context):
         dag_id=dag_id,
         database=database
     ))
+
+
+def check_table_existance(**context):
+    return 'skip_table_creation' if True else 'create_table'
 
 
 for dag_id in config:
@@ -36,6 +40,20 @@ for dag_id in config:
         ),
         dag=dag
     )
+    check_table_exist = BranchPythonOperator(
+        task_id='check_table_exist',
+        provide_context=True,
+        python_callable=check_table_existance,
+        dag=dag
+    )
+    create_table = DummyOperator(
+        task_id='create_table',
+        dag=dag
+    )
+    skip_table_creation = DummyOperator(
+        task_id='skip_table_creation',
+        dag=dag
+    )
     insert_new_row = DummyOperator(
         task_id='insert_new_row',
         dag=dag
@@ -44,7 +62,9 @@ for dag_id in config:
         task_id='query_the_table',
         dag=dag
     )
-    start_processing_tables_in_db >> insert_new_row >> query_the_table
+    start_processing_tables_in_db >> check_table_exist >> [create_table, skip_table_creation] >> \
+        insert_new_row >> query_the_table
+
     globals().update({dag_id: dag})
 
 
