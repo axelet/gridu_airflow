27772d462a5308f0b4565c9a63bd60f5b9a5012e
axelet <axelet3@gmail.com>
2019-12-30 14:25:19 +0300

    [GRID-AIRFLOW] 26. Add a Subdag

--- trigger_dag.py
+++ trigger_dag.py
@@ -1,13 +1,53 @@
 from airflow import DAG
+from airflow.models import Variable
 from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
 from datetime import datetime, timedelta
 
 
 dag_number = Variable.get('dag_number', default_var=5)
 run_flag_path = Variable.get('run_flag_path')
+
+
+def create_process_results(parent_dag_name, child_dag_name, schedule_interval, start_date):
+    subdag = DAG(
+        dag_id='{}.{}'.format(parent_dag_name, child_dag_name),
+        schedule_interval=schedule_interval,
+        start_date=start_date
+    )
+
+    sensor_triggered_dag = ExternalTaskSensor(
+        task_id='sensor_triggered_dag',
+        external_dag_id='dag_id_0',
+        external_task_id='None',
+        execution_date_fn=lambda exec_date: exec_date,
+        dag=subdag
+    )
+
+    print_result = PythonOperator(
+        task_id='print_result',
+        dag=subdag
+    )
+
+    remove_run_file = BashOperator(
+        task_id='remove_run_file',
+        bash_command='rm $AIRFLOW_HOME/{}'.format(run_flag_path),
+        dag=dag
+    )
+
+    finished_timestamp = BashOperator(
+        task_id='finished_{{ ts_nodash }}',
+        dag=subdag
+    )
+
+    sensor_triggered_dag >> print_result >> remove_run_file >> finished_timestamp
+
+    return subdag
+
 
 dag = DAG(
     dag_id='trigger_dag',
@@ -17,7 +57,7 @@ dag = DAG(
 
 check_run_file_exists = FileSensor(
     task_id='check_file_exists',
-    filepath='input/run',
+    filepath=run_flag_path,
     fs_conn_id='fs_airflow',
     poke_interval=20,
     dag=dag
@@ -29,10 +69,14 @@ trigger_dags = TriggerDagRunOperator(
     dag=dag
 )
 
-remove_run_file = BashOperator(
-    task_id='remove_run_file',
-    bash_command='rm $AIRFLOW_HOME/{}'.format(run_flag_path),
+process_results = SubDagOperator(
+    task_id='process_results',
+    subdag=create_process_results(parent_dag_name=dag.dag_id,
+                                  child_dag_name='process_results',
+                                  schedule_interval=dag.schedule_interval,
+                                  start_date=dag.start_date
+                                  ),
     dag=dag
 )
 
-check_run_file_exists >> trigger_dags >> remove_run_file
+check_run_file_exists >> trigger_dags >> process_results
